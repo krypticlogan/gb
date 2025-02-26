2d8aaa60202a1ae6b8820f1cb07af6cdadabc835
= <=>
2025-02-26 01:07:36 -0700

    boilerplate: cpu instructions, gpu, apu to be done

--- src/main.zig
+++ src/main.zig
@@ -10,11 +10,11 @@ const regID = enum(u3) {
     c,
     d,
     e,
-    f,
     h,
     l,
 };
 const FlagRegister = struct {
+    value: u8 = 0,
     zero: bool = false,
     subtract: bool = false,
     half_carry: bool = false,
@@ -25,26 +25,72 @@ const FlagRegister = struct {
     HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5,
     CARRY_FLAG_BYTE_POSITION: u8 = 4,
 
-    fn check(self: *@This(), byte: u8) @This() {
+    fn check(self: *@This(), byte: u8) void {
         self.zero = (byte >> self.ZERO_FLAG_BYTE_POSITION) & 0b1;
         self.subtract = (byte >> self.SUBTRACT_FLAG_BYTE_POSITION) & 0b1;
         self.half_carry = (byte >> self.HALF_CARRY_FLAG_BYTE_POSITION) & 0b1;
         self.carry = (byte >> self.CARRY_FLAG_BYTE_POSITION) & 0b1;
     }
 
-    fn tou8(self: *@This()) u8 {
-        return (if (self.zero) 1 else 0) << self.ZERO_FLAG_BYTE_POSITION |
+    fn write(self: *@This()) u8 {
+        self.value = (if (self.zero) 1 else 0) << self.ZERO_FLAG_BYTE_POSITION |
             (if (self.subtract) 1 else 0) << self.SUBTRACT_FLAG_BYTE_POSITION |
             (if (self.half_carry) 1 else 0) << self.HALF_CARRY_FLAG_BYTE_POSITION |
             (if (self.carry) 1 else 0 << self.CARRY_FLAG_BYTE_POSITION);
     }
 };
 
-const Instruction = enum { ADD };
+const InstructionSet = struct {
+    const table = [_]struct { u8, *const fn (*CPU) void }{
+        .{ 0x00, InstructionSet.NOP },
+        .{ 0x80, InstructionSet.ADDB },
+    };
+
+    const prefix_table = [_]struct { u8, *const fn (*CPU) void }{
+        .{ 0x00, InstructionSet.NOP },
+        .{ 0x80, InstructionSet.ADDB },
+    };
+
+    fn from_byte(byte: u8) !*const fn (*CPU) void {
+        inline for (table) |mapping| {
+            const code = mapping[0];
+            const ins = mapping[1];
+            if (code == byte) {
+                return ins;
+            }
+        }
+        // if (byte > 0 and byte < table.len) return table[byte][1]  else
+        print("\tbyte: 0x{x}\t", .{byte});
+        return error.IllegalByte;
+    }
+
+    fn from_prefixed_byte(byte: u8) !*const fn (*CPU) void {
+        inline for (prefix_table) |mapping| {
+            const code = mapping[0];
+            const ins = mapping[1];
+            if (code == byte) {
+                return ins;
+            }
+        }
+        // if (byte > 0 and byte < table.len) return table[byte][1]  else
+        print("\tbyte: 0x{x}\t", .{byte});
+        return error.IllegalByte;
+    }
+
+    fn NOP(cpu: *CPU) void {
+        print("NOP", .{});
+        cpu.pc += 1;
+    }
+    fn ADDB(cpu: *CPU) void {
+        print("ADDB", .{});
+        cpu.pc += 1;
+        // return 8;
+    }
+};
 /// Defines a GameBoy CPU: i8080 & Z80 hybrid chip
 const CPU = struct {
-    registers: [8]u8 = undefined,
-    flagRegister: FlagRegister = FlagRegister{},
+    registers: [7]u8 = undefined,
+    f: FlagRegister = FlagRegister{},
     pc: u16 = undefined,
     sp: u16 = undefined,
 
@@ -54,22 +100,134 @@ const CPU = struct {
         self.sp = 0;
     }
 
-    fn two_bit_set(self: *@This(), reg1: regID, value: u16) void {
+    fn load(self: *@This()) !void {
+        _ = self;
+    }
+
+    fn set_byte(self: *@This(), reg1: regID, value: u8) void {
+        self.registers[reg1] = value;
+    }
+
+    fn get_byte(self: *@This(), reg1: regID) u8 {
+        return self.registers[reg1];
+    }
+
+    fn set_word(self: *@This(), reg1: regID, value: u16) void {
         self.registers[reg1] = @truncate((value & 0xFF00) >> 8);
         self.registers[reg1 + 1] = @truncate(value & 0x00FF);
     }
 
-    fn two_bit_get(self: *@This(), reg1: regID) u16 {
-        return @as(u16, (self.registers[reg1] << 8)) | self.registers[reg1 + 1];
+    fn get_word(self: *@This(), reg1: regID) u16 {
+        return (@as(u16, self.registers[reg1]) << 8) | self.registers[reg1 + 1];
     }
+
+    fn execute(self: *@This(), gb: *GB) !void {
+        const byte = gb.memory[self.pc];
+        const prefixed = byte == 0xCB;
+        var ins: *const fn (*CPU) void = undefined;
+        if (prefixed) {
+            self.pc += 1;
+            ins = try InstructionSet.from_prefixed_byte(byte);
+        } else {
+            ins = try InstructionSet.from_byte(byte);
+        }
+        ins(self);
+        print("\n", .{});
+    }
+};
+
+const APU = struct {
+    // TODO: implement sound
+};
+
+const GPU = struct {
+    const VRAM_BEGIN = 0x8000;
+    const VRAM_END = 0x97FF;
+    const VRAM_SIZE = VRAM_END - VRAM_BEGIN + 1;
+
+    const Color = enum {
+        black,
+        dgray,
+        lgray,
+        white
+    };
+
+    const Tile = [8][8]Color;
+     fn empty_tile(self: *@This()) Tile {
+        _ = self;
+        var tile: Tile = undefined;
+        for (&tile) |*row| {
+            @memset(row, Color.black);
+        }
+        return tile;
+    }
+
+    vram: *[VRAM_SIZE]u8 = undefined,
+    tile_set: [384]Tile = undefined,
+
+    fn init(self: *@This(), gb: *GB) !void {
+        self.vram = gb.memory[VRAM_BEGIN..VRAM_END+1];
+        @memset(&self.tile_set, empty_tile(self));
+    } 
+
+    fn read_vram (self: *@This(), address: usize) u8 {
+        return self.vram[address];
+    }
+
+     fn write_vram (self: *@This(), address: usize, value: u8) void {
+        self.vram[address] = value;
+        if (address >= 0x1800) return;
+
+        // const normalized_address
+    }
+
+    fn vram_dump(self: *@This()) void {
+        print("VRAM dump: \n", .{});
+        for (self.vram, 0..VRAM_SIZE) |value, i| {
+            print("0x{x}", .{value});
+            if (i!=0 and i % 80 == 0) print("\n", .{});
+        }
+        print("\n", .{});
+    }
+
+   
 };
 
 const GB = struct {
     cpu: CPU = CPU{},
-
+    gpu: GPU = GPU{},
+    apu: APU = APU{},
+    memory: [0xFFFF]u8 = undefined,
     fn init(self: *@This()) !void {
+        @memset(&self.memory, 0);
         try self.cpu.init();
+        try self.gpu.init(self);
     }
+
+    fn cycle(self: *@This()) !void {
+        try self.cpu.execute(self);
+        //TODO: Update peripherals & timing
+    }
+
+    fn mem_dump(self: *@This()) void {
+        print("printing bytes:\n", .{});
+        for (self.memory, 0..self.memory.len) |value, i| {
+            print("0x{x}", .{value});
+            if (i!=0 and i % 64 == 0) print("\n", .{}) else continue;
+        }
+        print("\n", .{});
+    }
+
+    fn gfx_dump(self: *@This()) void {
+        print("Actual memspace dump: \n", .{});
+        for (self.memory[0x8000..0x97FF+1], 0..(0x97FF - 0x8000+1)) |value, i| {
+            print("0x{x}", .{value});
+            if (i!=0 and i % 80 == 0) print("\n", .{});
+        }
+        print("\n", .{});
+    }
+
+    
     // specs
     // CPU freq (TIMING)	4.194304 MHz1,	Up to 8.388608 MHz
     // Work RAM	8 KiB,	32 KiB3 (4 + 7 × 4 KiB)
@@ -91,5 +249,19 @@ pub fn main() !void {
         print("Couldn't inititalize GameBoy, Error: {any}\n", .{err});
         return;
     };
+
     print("GB init!\n", .{});
+    gb.memory[0] = 0x00;
+    gb.memory[1] = 0x80;
+
+    // gb.gpu.vram_dump();
+    // gb.gfx_dump();
+
+
+    print("\n", .{});
+    try gb.cycle();
+    try gb.cycle();
+
+    // const tile = gb.gpu.empty_tile();
+    print("{any}", .{gb.gpu.tile_set});
 }
